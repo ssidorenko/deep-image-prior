cbc9222ead79f836d2879e15c9a8c68f7a91087c
Semion Sidorenko <semion.sidorenko@gmail.com>
2018-08-08 16:48:57 +0200

    Save stddev exploration notebooks

--- utils/denoising_utils.py
+++ utils/denoising_utils.py
@@ -3,14 +3,18 @@ from .common_utils import *
 
 
         
-def get_noisy_image(img_np, sigma):
+def get_noisy_image(img_np, sigma, clip=True):
     """Adds Gaussian noise to an image.
     
     Args: 
         img_np: image, np.array with values from 0 to 1
         sigma: std of the noise
     """
-    img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    if clip:
+        img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    else:
+        img_noisy_np = (img_np + np.random.normal(scale=sigma, size=img_np.shape)).astype(np.float32)
+        
     img_noisy_pil = np_to_pil(img_noisy_np)
 
     return img_noisy_pil, img_noisy_np
